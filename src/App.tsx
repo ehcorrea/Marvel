04a5cf623ed0629a5eed2d28d5e3b209a6477359
Emilio Correa <ehcorrea60@gmail.com>
2022-05-28 15:49:44 -0300

    refactor: 💡 <App/>
    reseted config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+
 import { ThemeProvider } from 'styled-components/native';
 
 import theme from './styles/theme';
 
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView style={{ flex: 1 }} />
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 };
 
 export default App;
