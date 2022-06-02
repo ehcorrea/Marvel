a6f7bd5ead056afa981280fff9f2827f4e5d733d
Emilio Correa <ehcorrea60@gmail.com>
2022-06-02 12:47:38 -0300

    feat: 🚀 Alert

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { RootState } from '../store';
 import NoLoggedRoutes from './NoLoggedRoutes';
 import LoggedRoutes from './LoggedRoutes';
+import { Alert } from 'react-native';
 
 const Routes = () => {
   const { hasUser } = useSelector((state: RootState) => state.authReducer);
+
+  useEffect(() => {
+    Alert.alert('hasUser', String(hasUser));
+  }, [hasUser]);
 
   if (!hasUser) {
     return <NoLoggedRoutes />;
