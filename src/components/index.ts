3c2a33b3d6e0c50ede630b378d8e69475adf207c
Emilio Correa <ehcorrea60@gmail.com>
2022-06-04 00:53:03 -0300

    feat: 🚀 Home Routes
    add new tab itens component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as BannerSlider } from './BannerSlider';
 export { default as InfoCard } from './InfoCard';
 export { default as InfoCardSlider } from './InfoCardSlider';
 export { default as FavoriteButton } from './FavoriteButton';
+export { default as TabMenuItem } from './TabMenuItem';
--- src/routes/LoggedRoutes/Home/index.tsx
+++ src/routes/LoggedRoutes/Home/index.tsx
@@ -2,21 +2,65 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from '../../../views/Home';
+import theme from '../../../styles/theme';
+import { TabMenuItem } from '../../../components';
 
 const Tab = createBottomTabNavigator();
 
 const TabHome = () => {
   return (
     <Tab.Navigator
+      initialRouteName="home"
       screenOptions={{
         header: () => <></>,
+        tabBarStyle: {
+          position: 'relative',
+          backgroundColor: theme.colors.secondary,
+          height: 60,
+          borderTopWidth: 0,
+        },
       }}
     >
+      <Tab.Screen
+        name="profile"
+        component={Home}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ focused }) => (
+            <TabMenuItem
+              focused={focused}
+              iconSource={require('../../../assets/images/icon-profile.png')}
+              accessibilityLabel="profile tab button"
+            />
+          ),
+        }}
+      />
       <Tab.Screen
         name="home"
         component={Home}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: '',
+          tabBarIcon: ({ focused }) => (
+            <TabMenuItem
+              focused={focused}
+              iconSource={require('../../../assets/images/icon-home.png')}
+              accessibilityLabel="home tab button"
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="favorites"
+        component={Home}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ focused }) => (
+            <TabMenuItem
+              focused={focused}
+              iconSource={require('../../../assets/images/icon-favorite.png')}
+              accessibilityLabel="favorites tab  button"
+            />
+          ),
         }}
       />
     </Tab.Navigator>
