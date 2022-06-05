435cb1279bb5636bba57d36164db3d4c09068dab
Emilio Correa <ehcorrea60@gmail.com>
2022-06-05 20:33:13 -0300

    refactor: 💡 <FavoriteButton/>
    added styles for flatlist

--- src/components/FavoriteButton/styles.ts
+++ src/components/FavoriteButton/styles.ts
@@ -2,21 +2,20 @@ import Animated from 'react-native-reanimated';
 import styled, { css } from 'styled-components/native';
 
 export const Wrapper = styled.TouchableOpacity`
-  margin: 10px;
-  position: relative;
+  ${({ theme }) => css`
+    margin-right: ${theme.spacings.large};
+  `}
 `;
 
 export const View = styled(Animated.View)`
   ${({ theme }) => css`
     align-items: center;
     background-color: ${theme.colors.secondary};
+    border-radius: ${theme.border.radius.medium};
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     padding: ${theme.spacings.xxsmall} ${theme.spacings.xsmall};
-    border-radius: ${theme.border.radius.medium};
-    position: absolute;
-    left: 0;
     width: 100%;
   `}
 `;
