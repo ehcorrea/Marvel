b8736f495a8f27200a5cb22427fe4bfa183398f4
Emilio Correa <ehcorrea60@gmail.com>
2022-06-05 15:35:37 -0300

    feat: 🚀 <App/>
    added reanimated import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-reanimated';
 
 import theme from './styles/theme';
 import Routes from './routes';
