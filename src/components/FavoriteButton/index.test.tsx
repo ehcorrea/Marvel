f70490bb0b507783860eb52f0f437a27cbaa6348
Emilio Correa <ehcorrea60@gmail.com>
2022-06-08 18:57:00 -0300

    test: 💍 <FavoriteButton/>
    added test for animation props

--- src/components/FavoriteButton/index.test.tsx
+++ src/components/FavoriteButton/index.test.tsx
@@ -1,33 +1,55 @@
-import { fireEvent } from '@testing-library/react-native';
 import React from 'react';
+import { fireEvent } from '@testing-library/react-native';
 
 import FavoriteButton from '.';
 import { renderWithTheme } from '../../util/tests/helper';
 
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
+
 describe('<FavoriteButton/>', () => {
   it('should render element with default label and icon', () => {
-    const { getByLabelText, getByText } = renderWithTheme(<FavoriteButton />);
-    const labelText = 'Add Your Character';
+    const onPressMock = jest.fn();
+    const LABEL_TEXT = 'Add Your Character';
 
-    expect(getByLabelText(labelText)).toBeTruthy();
+    const { getByLabelText, getByText } = renderWithTheme(
+      <FavoriteButton onPress={onPressMock} />
+    );
+
+    const touchableElement = getByLabelText(LABEL_TEXT);
+
+    fireEvent.press(touchableElement);
+
+    expect(onPressMock).toBeCalled();
+    expect(getByLabelText(LABEL_TEXT)).toBeTruthy();
     expect(getByLabelText(`click add Character icon`)).toBeTruthy();
-    expect(getByText(labelText)).toBeTruthy();
+    expect(getByText(LABEL_TEXT)).toBeTruthy();
   });
 
   it('should render element with props', () => {
     const onPressMock = jest.fn();
+    const LABEL_TEXT = 'Hero Mock';
 
     const { getByLabelText, getByText } = renderWithTheme(
-      <FavoriteButton title="Hero Mock" onPress={onPressMock} />
+      <FavoriteButton title={LABEL_TEXT} onPress={onPressMock} />
     );
-    const labelText = 'Hero Mock';
-    const touchableElement = getByLabelText(labelText);
 
-    fireEvent.press(touchableElement);
+    expect(getByText(LABEL_TEXT)).toBeTruthy();
+    expect(getByLabelText(`click ${LABEL_TEXT} icon`)).toBeTruthy();
+  });
 
-    expect(touchableElement).toBeTruthy();
-    expect(onPressMock).toBeCalled();
-    expect(getByLabelText(`click ${labelText} icon`)).toBeTruthy();
-    expect(getByText(labelText)).toBeTruthy();
+  it('should wait animation', async () => {
+    const onPressMock = jest.fn();
+    const LABEL_TEXT = 'Hero Mock';
+
+    const { getByLabelText } = renderWithTheme(
+      <FavoriteButton title={LABEL_TEXT} onPress={onPressMock} withAnimation />
+    );
+
+    const touchableElement = getByLabelText(LABEL_TEXT);
+
+    await fireEvent.press(touchableElement);
+
+    expect(setTimeout).toHaveBeenLastCalledWith(onPressMock, 500);
   });
 });
