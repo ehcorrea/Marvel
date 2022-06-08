0715187249bd18980b388cc999916f4d95520dee
Emilio Correa <ehcorrea60@gmail.com>
2022-06-07 21:34:02 -0300

    feat: 🚀 view <Search/>
    initialized search view

---
+++ src/views/Search/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import * as S from './styles';
+
+const Search = () => (
+  <S.Wrapper>
+    <Text>Search</Text>
+  </S.Wrapper>
+);
+
+export default Search;
