f1411c681d0e7ab6994d17cf2daaa85db18eefd1
Emilio Correa <ehcorrea60@gmail.com>
2022-06-14 22:08:41 -0300

    feat: 🚀 <SearchInput/>
    created component with tests

--- src/components/SearchInput/index.test.tsx
+++ src/components/SearchInput/index.test.tsx
@@ -1,35 +1,73 @@
 import React from 'react';
+
 import { fireEvent } from '@testing-library/react-native';
 
 import { renderWithTheme } from '../../util/tests/helper';
-import SearchInput from '.';
+
+import SearchInput from './';
+
+jest.useFakeTimers();
+
+const BUTTON_LABEL = 'button to focus search field';
+const INPUT_LABEL = 'search field';
+const ICON_CLEAR_LABEL = 'clear search field';
+const ICON_ARROW_BACK = 'back button';
 
 describe('<SearchInput/>', () => {
-  it('shound render component and elements, change and clear input value', () => {
-    const onChangeMock = jest.fn();
+  it('should render component button', () => {
+    const handleChangeFocusMock = jest.fn();
+
     const { getByLabelText, queryByLabelText } = renderWithTheme(
-      <SearchInput onChangeText={onChangeMock} />
+      <SearchInput focused={false} handleChangeFocus={handleChangeFocusMock} />
     );
 
-    const textInput = getByLabelText('search input');
+    const button = getByLabelText(BUTTON_LABEL);
+    const input = queryByLabelText(INPUT_LABEL);
 
-    expect(getByLabelText('search input')).toHaveStyle({
-      flex: 1,
-      color: 'white',
-    });
+    expect(input).toBeFalsy();
 
-    //test icons
-    expect(getByLabelText('search icon')).toBeTruthy();
-    expect(queryByLabelText('clear icon')).toBeFalsy();
+    fireEvent.press(button);
 
-    //input text
-    fireEvent.changeText(textInput, 'onChangeText');
-    expect(textInput.props.value).toEqual('onChangeText');
-    expect(queryByLabelText('search icon')).toBeFalsy();
-    expect(queryByLabelText('clear icon')).toBeTruthy();
+    expect(handleChangeFocusMock).toBeCalled();
+  });
 
-    //clear input
-    fireEvent.press(getByLabelText('clear icon'));
-    expect(textInput.props.value).toEqual('');
+  it('should render input', () => {
+    let newInputValue = '';
+
+    const handleChangeFocusMock = jest.fn();
+    const onChangeTexMock = jest.fn((value: string) => (newInputValue = value));
+    const handleBackArrow = jest.fn();
+
+    const { queryByLabelText, getByLabelText } = renderWithTheme(
+      <SearchInput
+        focused
+        value={INPUT_LABEL}
+        handleChangeFocus={handleChangeFocusMock}
+        onChangeText={onChangeTexMock}
+        handleBackArrow={handleBackArrow}
+      />
+    );
+
+    const backButton = getByLabelText(ICON_ARROW_BACK);
+    const button = queryByLabelText(BUTTON_LABEL);
+    const clearButton = getByLabelText(ICON_CLEAR_LABEL);
+    const input = getByLabelText(INPUT_LABEL);
+
+    expect(backButton).toBeTruthy();
+    expect(button).toBeFalsy();
+    expect(clearButton).toBeTruthy();
+    expect(input.props.value).toEqual(INPUT_LABEL);
+
+    fireEvent.changeText(input, 'new value');
+    fireEvent.press(backButton);
+
+    expect(onChangeTexMock).toBeCalled();
+    expect(handleBackArrow).toBeCalled();
+    expect(newInputValue).toEqual('new value');
+
+    fireEvent.press(clearButton);
+
+    expect(onChangeTexMock).toBeCalledTimes(2);
+    expect(newInputValue).toEqual('');
   });
 });
--- src/components/SearchInput/styles.ts
+++ src/components/SearchInput/styles.ts
@@ -1,21 +1,56 @@
 import styled, { css } from 'styled-components/native';
+import Animated from 'react-native-reanimated';
 
-export const Wrapper = styled.View`
+import iconArrow from '../../assets/images/icon-arrow.png';
+import iconClear from '../../assets/images/icon-close.png';
+
+export const Wrapper = styled(Animated.View)`
   ${({ theme }) => css`
-    align-items: center;
+    border: solid 2px ${theme.colors.primary};
+    flex: 1;
+    max-height: 55px;
     background-color: ${theme.colors.secondary};
-    border-radius: ${theme.border.radius.large};
-    border: 2px solid ${theme.colors.primary};
-    display: flex;
-    flex-direction: row;
-    height: ${theme.spacings.huge};
-    padding: 0 ${theme.spacings.large};
   `}
 `;
 
-export const Icon = styled.Image`
+export const ButtonAnimatedView = styled(Animated.View)`
+  flex: 1;
+`;
+
+export const FocusButton = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonLabel = styled.Text`
   ${({ theme }) => css`
-    height: ${theme.spacings.xsmall};
-    width: ${theme.spacings.xsmall};
+    color: ${theme.colors.white};
+    font-weight: ${theme.font.wight.bold};
+  `}
+`;
+
+export const InputAnimatedView = styled(Animated.View)`
+  ${({ theme }) => css`
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    padding: 0 ${theme.spacings.xxsmall};
+  `}
+`;
+
+export const IconArrow = styled.Image.attrs(() => ({ source: iconArrow }))`
+  ${({ theme }) => css`
+    width: ${theme.spacings.large};
+    height: ${theme.spacings.large};
+    margin: 0 ${theme.spacings.xxsmall};
+  `}
+`;
+
+export const IconClear = styled.Image.attrs(() => ({ source: iconClear }))`
+  ${({ theme }) => css`
+    width: ${theme.spacings.large};
+    height: ${theme.spacings.large};
+    margin: 0 ${theme.spacings.xxsmall};
   `}
 `;
